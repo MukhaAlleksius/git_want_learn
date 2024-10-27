ca2db70c6167651df4953852a9c809d79621b5cd
MukhaAlleksius <mukha.alleksius@mail.ru>
2024-10-27 14:04:04 +0300

    Add test_calculator

--- calculator.py
+++ calculator.py
@@ -1,9 +1,11 @@
-
-
 class Calculator:
 
-    last_res=0
+    last_res = 0
 
-    def sum(self,n1,n2):
-          self.last_res=n1+n2
-          return self.last_res
+    def sum(self, n1, n2):
+        self.last_res = n1 + n2
+        return self.last_res
+
+    def minus(self, n1, n2):
+        self.last_res = n1 - n2
+        return self.last_res
---
+++ test_calculator.py
@@ -0,0 +1,3 @@
+from calculator import calculator
+
+print(sum(10,5))
